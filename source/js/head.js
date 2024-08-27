de3601970a2b95fcbcd9a5dac49884df99875714
dragonjay-lyj <liujinlisb@gmail.com>
2024-08-27 11:24:07 +0800

    modified:   _config.anzhiyu.yml 	new file:   source/js/head.js

---
+++ source/js/head.js
@@ -0,0 +1,19 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?56d98fada62ee1945b1b8abf83552076";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ntcgsys234");
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-WL6KQMHFSK');
